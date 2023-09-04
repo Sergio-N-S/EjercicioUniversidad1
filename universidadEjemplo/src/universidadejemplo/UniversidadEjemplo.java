d06b5460390867e5d6168bbba03782e79e0e8373
Sergio-N-S <119826013+Sergio-N-S@users.noreply.github.com>
2023-09-04 11:49:51 -0300

    commit del proyecto

---
+++ universidadEjemplo/src/universidadejemplo/UniversidadEjemplo.java
@@ -0,0 +1,26 @@
+
+package universidadejemplo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+public class UniversidadEjemplo {
+    public static void main(String[] args) {
+        
+        try {
+            //cargar driver
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+            Connection conexion =DriverManager.getConnection("jdbc:mariadb://localhost:3306/universidad_ulp", "root","");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar el driver "+ex.getMessage());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion "+ex.getMessage());
+        }
+    }
+
+}
